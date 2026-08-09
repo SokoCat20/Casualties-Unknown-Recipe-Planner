d3ddd720ce15d76f3dbad4e88e0cf9ea1fcd5eba
SokoCat20 <egyptthecat5203@gmail.com>
2026-08-08 21:30:25 -0400

    small edits to the site

--- assets/itemNode.js
+++ assets/itemNode.js
@@ -9,7 +9,7 @@ class ItemNode {
     GenerateNode() {
         this.node = document.createElement("div");  // This div will hold the entire node
         this.node.setAttribute("style", `height: ${this.height}px; width: ${this.width}px;`);
-        this.node.id = "recipeNode";
+        this.node.id = "recipe-node";
         this.node.classList.add("divdrag");
         this.node.appendChild(document.createTextNode("this is a node"));
     }
